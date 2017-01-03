2705a4f9df38f13594c1cb953bb6ab5da2d63890
Murry Jeong <murry.jeong@streamlyzer.com>
2017-01-04 00:26:06 +0900

    Added debugging log messages

--- src/main/java/comchangs/toyproject/fds/Detector.java
+++ src/main/java/comchangs/toyproject/fds/Detector.java
@@ -28,62 +28,69 @@ public class Detector implements Runnable
   public void process() {
 
     try {
-      //TODO: DetectionTask by rules
       Object event = queue.take();
+      logger.debug("Received: " + event);
       long accountNumber = 0;
+      AccountTrackingInformation accountTrackingInformation = null;
 
-      if(accountNumber != 0) {
+      if (event instanceof NewAccountEvent) {
+        logger.debug("NewAccountEvent: " + accountNumber);
+        accountNumber = ((NewAccountEvent) event).getAccountNumber();
+        accountTrackingMap.put(
+            accountNumber,
+            new AccountTrackingInformation(((NewAccountEvent) event).getIssuedTimestamp())
+        );
+        logger.debug(accountTrackingMap);
+      } else if (event instanceof DepositEvent) {
+        logger.debug("DepositEvent: " + accountNumber);
+        accountNumber = ((DepositEvent) event).getAccountNumber();
+        accountTrackingInformation = (AccountTrackingInformation) accountTrackingMap.get(accountNumber);
+        accountTrackingInformation.deposit(((DepositEvent) event).getDepositAmount());
 
-        if (event instanceof NewAccountEvent) {
-          accountNumber = ((NewAccountEvent) event).getAccountNumber();
+        if (isPassRules(accountTrackingInformation)) {
           accountTrackingMap.put(
               accountNumber,
-              new AccountTrackingInformation(((NewAccountEvent) event).getIssuedTimestamp())
+              new AccountTrackingInformation(((DepositEvent) event).getIssuedTimestamp())
           );
-        } else if (event instanceof DepositEvent) {
-          accountNumber = ((NewAccountEvent) event).getAccountNumber();
-          AccountTrackingInformation accountTrackingInformation = (AccountTrackingInformation) accountTrackingMap.get(accountNumber);
-          accountTrackingInformation.deposit(((DepositEvent) event).getDepositAmount());
+        }
+      } else if (event instanceof WithdrawEvent) {
+        logger.debug("WithdrawEvent: " + accountNumber);
+        accountNumber = ((WithdrawEvent) event).getAccountNumber();
+        accountTrackingInformation = (AccountTrackingInformation) accountTrackingMap.get(accountNumber);
+        accountTrackingInformation.withdraw(((WithdrawEvent) event).getWithdrawalAmount());
 
-          if (isPassRules(accountTrackingInformation)) {
-            accountTrackingMap.put(
-                accountNumber,
-                new AccountTrackingInformation(((DepositEvent) event).getIssuedTimestamp())
-            );
-          }
-        } else if (event instanceof WithdrawEvent) {
-          accountNumber = ((NewAccountEvent) event).getAccountNumber();
-          AccountTrackingInformation accountTrackingInformation = (AccountTrackingInformation) accountTrackingMap.get(accountNumber);
-          accountTrackingInformation.withdraw(((WithdrawEvent) event).getWithdrawalAmount());
+        if (isPassRules(accountTrackingInformation)) {
+          accountTrackingMap.put(
+              accountNumber,
+              new AccountTrackingInformation(((WithdrawEvent) event).getIssuedTimestamp())
+          );
+        }
+      } else if (event instanceof RemittanceEvent) {
+        logger.debug("RemittanceEvent: " + accountNumber);
+        accountNumber = ((RemittanceEvent) event).getAccountNumber();
+        accountTrackingInformation = (AccountTrackingInformation) accountTrackingMap.get(accountNumber);
+        accountTrackingInformation.remittance(((RemittanceEvent) event).getRemittanceAmount());
 
-          if (isPassRules(accountTrackingInformation)) {
-            accountTrackingMap.put(
-                accountNumber,
-                new AccountTrackingInformation(((WithdrawEvent) event).getIssuedTimestamp())
-            );
-          }
-        } else if (event instanceof RemittanceEvent) {
-          accountNumber = ((NewAccountEvent) event).getAccountNumber();
-          AccountTrackingInformation accountTrackingInformation = (AccountTrackingInformation) accountTrackingMap.get(accountNumber);
-          accountTrackingInformation.remittance(((RemittanceEvent) event).getRemittanceAmount());
-
-          if (isPassRules(accountTrackingInformation)) {
-            accountTrackingMap.put(
-                accountNumber,
-                new AccountTrackingInformation(((RemittanceEvent) event).getIssuedTimestamp())
-            );
-          }
+        if (isPassRules(accountTrackingInformation)) {
+          accountTrackingMap.put(
+              accountNumber,
+              new AccountTrackingInformation(((RemittanceEvent) event).getIssuedTimestamp())
+          );
         }
       }
+
     } catch (InterruptedException e) {
       logger.info( "InterruptedException: " + e.getStackTrace() );
+      e.printStackTrace();
     } catch (Exception e) {
       logger.error("Exception: " + e.getStackTrace());
+      e.printStackTrace();
     }
   }
 
   private boolean isPassRules(AccountTrackingInformation accountTrackingInformation) {
-
+    //TODO: How to check by rules
+    logger.info("Pass: " + accountTrackingInformation);
     return true;
   }
 
